b2363f3d68ae605c5d924975a4617cb87dabc911
Joe Littlejohn <joe.littlejohn@nokia.com>
2013-07-05 15:59:59 +0100

    Update to non-SNAPSHOT version as lein now requires a RELEASE version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljskel/lein-template "0.1.0-SNAPSHOT"
+(defproject cljskel/lein-template "0.1.0"
   :description "Skeleton project, a template that can be used to create a new Clojure web app implementing /1.x/ping and /1.x/status"
   :url "http://wikis.in.nokia.com/Clojure/ClojureSkeleton"
   :eval-in-leiningen true)
