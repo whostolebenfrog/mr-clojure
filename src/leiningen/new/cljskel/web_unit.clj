d79892085c7cab8416140ef5ee279e05bd0e2285
Neil Prosser <neil.prosser@gmail.com>
2013-10-30 16:56:39 +0000

    Remove overwritten function

--- src/leiningen/new/cljskel/web_unit.clj
+++ src/leiningen/new/cljskel/web_unit.clj
@@ -3,13 +3,6 @@
   (:use [{{name}}.web]
         [midje.sweet]))
 
-(defn request
-  [method resource & {:as others}]
-  (routes (merge {:request-method method
-                  :uri resource} (update-in others [:body]
-                                         #(java.io.ByteArrayInputStream.
-                                           (.getBytes (json/generate-string %)))))))
-
 (defn request [method resource]
   (routes {:request-method method
            :uri resource } ))
