9825707a09230a0ab494a002c8648ab0426b037e
Neil Prosser <neil.prosser@gmail.com>
2014-02-07 17:19:10 +0000

    Update dependencies

--- src/leiningen/new/cljskel/project.clj
+++ src/leiningen/new/cljskel/project.clj
@@ -2,43 +2,43 @@
   :description "{{upper-name}} service"
   :url "http://wikis.in.nokia.com/NokiaMusicArchitecture/{{upper-name}}"
 
-  :dependencies [[ch.qos.logback/logback-classic "1.0.13"]
-                 [cheshire "5.2.0"]
-                 [clj-http "0.7.7"]
+  :dependencies [[ch.qos.logback/logback-classic "1.1.1"]
+                 [cheshire "5.3.1"]
+                 [clj-http "0.7.9"]
                  [clj-time "0.6.0"]
                  [com.ovi.common.logging/logback-appender "0.0.45"]
-                 [com.ovi.common.metrics/metrics-graphite "2.1.22"]
+                 [com.ovi.common.metrics/metrics-graphite "2.1.23"]
                  [com.yammer.metrics/metrics-logback "2.2.0"]
                  [compojure "1.1.6" :exclusions [javax.servlet/servlet-api]]
                  [environ "0.4.0"]
                  [metrics-clojure "1.0.1"]
                  [metrics-clojure-ring "1.0.1"]
-                 [nokia/instrumented-ring-jetty-adapter "0.1.5"]
+                 [nokia/instrumented-ring-jetty-adapter "0.1.7"]
                  [nokia/ring-utils "1.2.1"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.3"]
+                 [org.clojure/data.json "0.2.4"]
                  [org.clojure/data.xml "0.0.7"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.eclipse.jetty/jetty-server "8.1.14.v20131031"]
-                 [org.slf4j/slf4j-api "1.7.5"]
-                 [org.slf4j/jcl-over-slf4j "1.7.5"]
-                 [org.slf4j/jul-to-slf4j "1.7.5"]
-                 [org.slf4j/log4j-over-slf4j "1.7.5"]
-                 [ring-middleware-format "0.3.1"]
+                 [org.slf4j/slf4j-api "1.7.6"]
+                 [org.slf4j/jcl-over-slf4j "1.7.6"]
+                 [org.slf4j/jul-to-slf4j "1.7.6"]
+                 [org.slf4j/log4j-over-slf4j "1.7.6"]
+                 [ring-middleware-format "0.3.2"]
                  [ring/ring-jetty-adapter "1.2.1" :exclusions [org.eclipse.jetty/jetty-server]]]
 
  :exclusions [commons-logging
               log4j]
 
-  :profiles {:dev {:dependencies [[com.github.rest-driver/rest-client-driver "1.1.32"
+  :profiles {:dev {:dependencies [[com.github.rest-driver/rest-client-driver "1.1.36"
                                    :exclusions [org.slf4j/slf4j-nop
                                                 javax.servlet/servlet-api
                                                 org.eclipse.jetty.orbit/javax.servlet]]
                                   [clj-http-fake "0.4.1"]
                                   [junit "4.11"]
                                   [midje "1.6.0"]
-                                  [rest-cljer "0.1.7"]]
+                                  [rest-cljer "0.1.11"]]
                    :plugins [[lein-rpm "0.0.5"]
                              [lein-midje "3.1.3"]
                              [jonase/kibit "0.0.8"]]}}
