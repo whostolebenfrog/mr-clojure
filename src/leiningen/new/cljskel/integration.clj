c70477d9243a6737ac85985b2332d2f102ff9dc7
Joe Littlejohn <joe.littlejohn@nokia.com>
2014-02-14 15:39:15 +0000

    Make clj-http alias consistent across acceptance & integration

--- src/leiningen/new/cljskel/integration.clj
+++ src/leiningen/new/cljskel/integration.clj
@@ -1,6 +1,6 @@
 (ns {{name}}.integration
   (:require [{{name}}.test-common :refer :all])
-  (:require [clj-http.client :as client]
+  (:require [clj-http.client :as http]
             [midje.sweet :refer :all]
             [environ.core :refer [env]])
   (:import [java.util UUID]))
@@ -9,9 +9,9 @@
  :integration
 
    (fact "Ping resource returns 200 HTTP response"
-         (let [response (client/get (url+ "/ping")  {:throw-exceptions false})]
+         (let [response (http/get (url+ "/ping")  {:throw-exceptions false})]
            response => (contains {:status 200})))
 
  (fact "Healthcheck resource returns 200 HTTP response"
-       (let [response (client/get (url+ "/healthcheck") {:throw-exceptions false})]
+       (let [response (http/get (url+ "/healthcheck") {:throw-exceptions false})]
          response => (contains {:status 200}))))
