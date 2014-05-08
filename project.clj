a85cd90dcf5c6ba10cb1c81b40d5e0fab62e117a
Neil Prosser <neil.prosser@nokia.com>
2014-05-08 09:04:26 -0700

    Correct project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject cljskel/lein-template "0.1.0"
-  :description "Skeleton project, a template that can be used to create a new Clojure web app implementing /1.x/ping and /1.x/status"
+  :description "Skeleton project, a template that can be used to create a new Clojure web app implementing /ping and /healthcheck"
   :url "http://wikis.in.nokia.com/Clojure/ClojureSkeleton"
   :eval-in-leiningen true)
