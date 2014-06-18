58d948d4fba4b22c0340c1e3a6582d65925d4bb1
Ben Griffiths <benjamin.griffiths@nokia.com>
2014-06-18 14:39:07 +0100

    Add better request method

--- src/leiningen/new/cljskel/web_unit.clj
+++ src/leiningen/new/cljskel/web_unit.clj
@@ -1,11 +1,23 @@
 (ns {{name}}.unit.web
-    (:require [{{name}}.web :refer :all]
-              [cheshire.core :as json]
-              [midje.sweet :refer :all]))
+  (:require [{{name}}.web :refer :all]
+            [cheshire.core :as json]
+            [midje.sweet :refer :all])
+  (:import [java.io ByteArrayInputStream InputStream]))
 
-(defn request [method resource]
-  (app {:request-method method
-        :uri resource}))
+(defn request
+  "Creates a compojure request map and applies it to our routes.
+   Accepets method, resource and optionally an extended map"
+  [method resource & [{:keys [params body content-type]
+                       :or {:params {}}}]]
+  (let [{:keys [body] :as res}
+        (app (merge {:request-method method
+                     :uri resource
+                     :params params}
+                    (when body {:body (-> body json/generate-string .getBytes ByteArrayInputStream.)})
+                    (when content-type {:content-type content-type})))]
+    (cond-> res
+            (instance? InputStream body)
+            (assoc :body (json/parse-string (slurp body) true)))))
 
 (fact-group
  :unit
