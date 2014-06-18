bc6d20d7fcb122f39dd79bfeae9df228bc532671
Ben Griffiths <benjamin.griffiths@nokia.com>
2014-06-18 14:39:21 +0100

    Pre-condition for application/json content type on json-body

--- src/leiningen/new/cljskel/test_common.clj
+++ src/leiningen/new/cljskel/test_common.clj
@@ -1,12 +1,15 @@
 (ns {{name}}.test-common
-    (:require [cheshire.core :as json]
-              [clojure.string :as str]
-              [environ.core :refer [env]]))
+  (:require [cheshire.core :as json]
+            [clojure.string :as str]
+            [environ.core :refer [env]]))
 
 (defn url+ [& suffix]
   (apply str (format (env :service-url) (env :service-port)) suffix))
 
 (defn json-body
-  "Reads the body of the request as json and parses it into a map with keywords."
+  "Reads the body of the request as json and parses it into a map with keywords.
+
+   Fails pre-condition if content type is not application/json."
   [resp]
+  {:pre [(re-matches #"application/json.+" (get-in resp [:headers "content-type"]))]}
   (json/parse-string (:body resp) true))
