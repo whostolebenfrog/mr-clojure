180c08d4462160349d53c8cd0859e47757ec5ec3
Neil Prosser <neil.prosser@gmail.com>
2014-10-08 13:57:45 +0100

    Use Logstash encoder instead of logging service

--- src/leiningen/new/cljskel/project.clj
+++ src/leiningen/new/cljskel/project.clj
@@ -1,16 +1,15 @@
 (defproject {{name}} "1.0.0-SNAPSHOT"
   :description "{{upper-name}} service"
-  :url "http://wikis.in.nokia.com/NokiaMusicArchitecture/{{upper-name}}"
 
   :dependencies [[ch.qos.logback/logback-classic "1.1.2"]
                  [cheshire "5.3.1"]
                  [clj-http "0.7.9"]
                  [clj-time "0.8.0"]
-                 [com.ovi.common.logging/logback-appender "0.0.47"]
                  [compojure "1.2.0"]
                  [environ "1.0.0"]
                  [mixradio/instrumented-ring-jetty-adapter "1.0.0"]
                  [mixradio/radix "1.0.2"]
+                 [net.logstash.logback/logstash-logback-encoder "3.2"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ring-middleware-format "0.4.0"]]
@@ -30,11 +29,10 @@
                              [lein-midje "3.1.3"]
                              [lein-kibit "0.0.8"]]}}
 
-  :plugins [[lein-ring "0.8.10"]
-            [lein-environ "0.5.0"]
+  :plugins [[lein-ring "0.8.12"]
+            [lein-environ "1.0.0"]
             [lein-release "1.0.73"]]
 
-  ;; development token values
   :env {:environment-name "poke"
         :service-name "{{name}}"
         :service-port "8080"
@@ -44,24 +42,18 @@
         :environment-entertainment-graphite-port "8080"
         :service-graphite-post-interval "1"
         :service-graphite-post-unit "MINUTES"
-        :service-graphite-enabled "ENABLED"
+        :service-graphite-enabled "DISABLED"
         :service-production "false"
         :service-jetty-gracefulshutdown-millis "5000"}
 
-  :lein-release {:release-tasks [:clean :uberjar :pom :rpm]
-                 :clojars-url "clojars@clojars.brislabs.com:"}
+  :lein-release {:release-tasks [:clean :uberjar :pom :rpm]}
 
   :ring {:handler {{name}}.web/app
          :main {{name}}.setup
-         :port ~(Integer.  (get (System/getenv) "SERVICE_PORT" "8080"))
+         :port ~(Integer/valueOf (get (System/getenv) "SERVICE_PORT" "8080"))
          :init {{name}}.setup/setup
          :browser-uri "/healthcheck"
          :nrepl {:start? true}}
-
-  :repositories {"internal-clojars"
-                 "http://clojars.brislabs.com/repo"
-                 "rm.brislabs.com"
-                 "http://rm.brislabs.com/nexus/content/groups/all-releases"}
 
   :uberjar-name "{{lower-name}}.jar"
 
