f7942811ca8c3282852728c171b6afdf8a8b0a7e
Neil Prosser <neil.prosser@gmail.com>
2014-10-09 12:22:56 +0100

    Update properties

--- src/leiningen/new/cljskel/project.clj
+++ src/leiningen/new/cljskel/project.clj
@@ -8,7 +8,7 @@
                  [compojure "1.2.0"]
                  [environ "1.0.0"]
                  [mixradio/instrumented-ring-jetty-adapter "1.0.0"]
-                 [mixradio/radix "1.0.2"]
+                 [mixradio/radix "1.0.3"]
                  [net.logstash.logback/logstash-logback-encoder "3.2"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
@@ -25,26 +25,30 @@
                                   [junit "4.11"]
                                   [midje "1.6.3"]
                                   [rest-cljer "0.1.11"]]
-                   :plugins [[lein-rpm "0.0.5"]
+                   :plugins [[lein-kibit "0.0.8"]
                              [lein-midje "3.1.3"]
-                             [lein-kibit "0.0.8"]]}}
+                             [lein-rpm "0.0.5"]]}}
 
-  :plugins [[lein-ring "0.8.12"]
-            [lein-environ "1.0.0"]
-            [lein-release "1.0.5"]]
+  :plugins [[lein-environ "1.0.0"]
+            [lein-release "1.0.5"]
+            [lein-ring "0.8.12"]]
 
   :env {:environment-name "poke"
-        :service-name "{{name}}"
-        :service-port "8080"
-        :service-url "http://localhost:%s"
-        :restdriver-port "8081"
+        :graphite-enabled false
         :graphite-host ""
-        :graphite-port "8080"
-        :graphite-post-interval "1"
-        :graphite-post-unit "MINUTES"
-        :graphite-enabled "DISABLED"
-        :production "false"
-        :shutdown-timeout-millis "5000"}
+        :graphite-port 2003
+        :graphite-post-interval-seconds 60
+        :logging-filethreshold "info"
+        :logging-level "info"
+        :logging-path "/tmp"
+        :logging-stashthreshold "off"
+        :production false
+        :service-name "{{name}}"
+        :service-port 8080
+        :service-url "http://localhost:%s"
+        :shutdown-timeout-millis 5000
+        :restdriver-port 8081
+        :threads 254}
 
   :lein-release {:deploy-via :shell
                  :shell ["lein" "do" "clean," "uberjar," "pom," "rpm"]}
