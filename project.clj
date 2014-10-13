e445ec812fd80d3c838b5f3e680584becc8e4816
Ben Griffiths <benjamin.griffiths@nokia.com>
2014-10-13 14:22:09 +0100

    Version 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mixradio/mr-clojure "1.0.2-SNAPSHOT"
+(defproject mixradio/mr-clojure "1.0.2"
   :description "MixRadio clojure skeleton template for Leiningen. Generates a Clojure HTTP REST service in the style of MixRadio"
   :url "http://github.com/mixradio/mr-clojure"
   :eval-in-leiningen true
