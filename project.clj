ad9ad127d0e95ba94999941d7ed5a85beadbaa94
Ben Griffiths <benjamin.griffiths@microsoft.com>
2015-03-02 14:59:05 +0000

    Version 1.0.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mr-clojure/lein-template "1.0.7"
+(defproject mr-clojure/lein-template "1.0.8-SNAPSHOT"
   :description "MixRadio clojure skeleton template for Leiningen. Generates a Clojure HTTP REST service in the style of MixRadio"
   :url "http://github.com/mixradio/mr-clojure"
   :eval-in-leiningen true
